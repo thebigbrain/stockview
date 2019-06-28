ade98b897796ed6074ceb033ee3fbb33849c4a12
thebigbrain <uibrick@126.com>
2019-06-28 13:51:47 +0800

    .

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ViewLayout from 'components/ViewLayout/ViewLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <ViewLayout/>
+    </div>
+  );
+}
+
+export default App;
